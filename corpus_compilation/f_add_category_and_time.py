e807d1fa5ff56ba239db86091bd24d289ed303ed
cooperchris17 <87263328+cooperchris17@users.noreply.github.com>
2023-02-09 12:43:42 +0900

    Update f_add_category_and_time.py

--- corpus_compilation/f_add_category_and_time.py
+++ corpus_compilation/f_add_category_and_time.py
@@ -1,3 +1,12 @@
+'''
+This script adds 2 new columns to the stats dataframe
+
+'category' - adds a category label from the number e.g. 'Education' from 27
+- the vid_cat_tups.py file must be in the same folder when running this script
+
+'time_in_seconds' - converts the video length to seconds 
+'''
+
 import pandas as pd
 import isodate
 from vid_cat_tups import vid_cats
